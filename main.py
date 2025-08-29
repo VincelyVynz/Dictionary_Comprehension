6c4bc77e0114e30309c475e3d08d2010b761d76b
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-08-29 11:31:05 +0530

    added inverted dict

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ Syntax:
     newdict = {key_expr: value_expr for item in iterable if condition == True}
 
 """
+
+
+new_dict = {
+    "name": "Alice",
+    "age": 22,
+    "city": "New York",
+    "state": "NY",
+    "country": "USA"
+}
+
+inverted_dict = { v:k for k, v in new_dict.items() }
+
+print(inverted_dict)
+
+
+# Creating a dictionary from two lists:
